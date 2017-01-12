fb1a1ba4acba380a36e6bb17e5eea6e5937fea90
Andrey Belikov <omgumail@yandex.ru>
2017-01-12 16:28:19 +0600

    add Date onstart

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,10 @@
 package sample;
 
 import javafx.fxml.FXML;
-
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
 
 public class Controller {
 
@@ -10,11 +12,24 @@ public class Controller {
     private TextField market;
     @FXML
     private TextField section;
+    @FXML
+    private TextField transaction;
+    @FXML
+    private DatePicker purchaseDate;
+
+    //public CheckNode node = new CheckNode(1);
 
     @FXML
     public void onClickMarket(){
         System.out.println(market.getOnMouseClicked());
+        purchaseDate.setValue(LocalDate.now());
         market.setText("Готово");
+    }
+
+    @FXML
+    public void setTransaction(){
+        CheckNode node = new CheckNode(Integer.parseInt(transaction.getText()));
+        transaction.setText("0");
     }
 
 }
